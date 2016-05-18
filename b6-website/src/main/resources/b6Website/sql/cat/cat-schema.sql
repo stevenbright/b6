afb1d62ff9617db172251a64f6871f051e336b26
Steven <vstrength@gmail.com>
2016-05-17 22:20:07 -0700

    Adding cat-schema

--- b6-website/src/main/resources/b6Website/sql/cat/cat-schema.sql
+++ b6-website/src/main/resources/b6Website/sql/cat/cat-schema.sql
@@ -1,3 +1,4 @@
+
 --
 -- Tables
 --
@@ -7,3 +8,33 @@ CREATE TABLE entity_type (
   name              CHAR(32) NOT NULL,
   CONSTRAINT uq_entity_type_name UNIQUE (name)
 );
+
+CREATE TABLE item (
+  id                INTEGER PRIMARY KEY,
+  title             VARCHAR(1024) NOT NULL,
+  type_id           INTEGER NOT NULL,
+  CONSTRAINT fk_item_type FOREIGN KEY (type_id) REFERENCES entity_type(id) ON DELETE CASCADE
+);
+
+CREATE TABLE item_book (
+  item_id           INTEGER PRIMARY KEY,
+  CONSTRAINT fk_item_book_id FOREIGN KEY (item_id) REFERENCES item(id) ON DELETE CASCADE
+);
+
+CREATE TABLE item_relation (
+  lhs               INTEGER NOT NULL,
+  rhs               INTEGER NOT NULL,
+  type_id           INTEGER NOT NULL,
+  CONSTRAINT pk_item_relation PRIMARY KEY (lhs, rhs, type_id),
+  CONSTRAINT fk_item_relation_lhs FOREIGN KEY (lhs) REFERENCES item(id) ON DELETE CASCADE,
+  CONSTRAINT fk_item_relation_rhs FOREIGN KEY (rhs) REFERENCES item(id) ON DELETE CASCADE,
+  CONSTRAINT fk_item_relation_type FOREIGN KEY (type_id) REFERENCES entity_type(id) ON DELETE CASCADE
+);
+
+
+--
+-- Sequences
+--
+
+CREATE SEQUENCE seq_entity_type       START WITH 100;
+CREATE SEQUENCE seq_item              START WITH 1000;
